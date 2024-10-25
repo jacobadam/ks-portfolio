89399c8cd3c67996ffa9e7fdd31c59fbb9e8ed8a
jacobadam <jacobanevitt@gmail.com>
2024-10-25 16:49:47 -0700

    add links and linkedin logo

--- src/NavBar.js
+++ src/NavBar.js
@@ -13,7 +13,8 @@ const NavBar = () => {
         <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
           <div>Katie Shevlin</div>
 
-          <div className="flex flex-1 items-center justify-end md:justify-center">
+          {/* Centered Links */}
+          <div className="flex flex-1 items-center justify-center">
             <nav aria-label="Global" className="hidden md:block">
               <ul className="flex items-center gap-6 text-sm">
                 <li>
@@ -21,65 +22,71 @@ const NavBar = () => {
                     className="text-gray-500 transition hover:text-gray-500/75"
                     href="/about"
                   >
-                    {" "}
-                    About{" "}
+                    About
                   </a>
                 </li>
-
                 <li>
                   <a
                     className="text-gray-500 transition hover:text-gray-500/75"
-                    href="services"
+                    href="/services"
                   >
-                    {" "}
-                    Services{" "}
+                    Services
                   </a>
                 </li>
-
                 <li>
                   <a
                     className="text-gray-500 transition hover:text-gray-500/75"
-                    href="portfolio"
+                    href="/portfolio"
                   >
-                    {" "}
-                    Portfolio{" "}
+                    Portfolio
                   </a>
                 </li>
-
                 <li>
                   <a
                     className="text-gray-500 transition hover:text-gray-500/75"
-                    href="contact"
+                    href="/contact"
                   >
-                    {" "}
-                    Contact{" "}
+                    Contact
                   </a>
                 </li>
               </ul>
             </nav>
+          </div>
 
-            <div className="flex items-center gap-4">
-              <button
-                onClick={toggleMenu}
-                className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
+          <div className="ml-auto flex items-center gap-4">
+            <a
+              href="https://www.linkedin.com/in/katie-shevlin90/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={`${process.env.PUBLIC_URL}/linkedin-logo.webp`}
+                alt="LinkedIn"
+                className="h-6 w-6"
+              />
+            </a>
+
+            {/* Mobile Menu Button */}
+            <button
+              onClick={toggleMenu}
+              className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
+            >
+              <span className="sr-only">Toggle menu</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
               >
-                <span className="sr-only">Toggle menu</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
-            </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
           </div>
         </div>
 
